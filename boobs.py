f33b08b0e64e86c7128a2ff5c6184d3966c0f28b
Ruslan panov <parusoft@yandex.ru>
2013-08-31 16:22:23 +0400

    Url exist check

--- boobs.py
+++ boobs.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
-import random, webbrowser;
+import random, webbrowser, urllib2;
 
-def getBoobs():
+def getUrl():
 	number = random.randrange(7600); 
 	url =    "http://media.oboobs.ru/boobs/0"+str(number)+".jpg"
-	webbrowser.open_new(url);
+	return url;
 
-getBoobs();
+def getBoobs(url):
+	result = 1;
+	try:
+    		urllib2.urlopen(url);
+    		webbrowser.open_new(url);
+    
+    	except urllib2.HTTPError, e:
+    		if e.code != 200:
+    			getBoobs(getUrl());
+
+getBoobs(getUrl());
